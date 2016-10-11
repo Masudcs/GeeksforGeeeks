4a0fcb330b373db981183e0dfb0a82db3c818695
kohinur1 <masud.pust16@gmail.com>
2016-10-11 17:09:46 +0600

    Add files via upload

---
+++ Cube root of a number basic.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T,N;
+    cin>>T;
+    while(T--)
+    {
+        cin>>N;
+        float r=pow(N,1.0/3.0);
+        r=(int)r;
+        cout<<r<<endl;
+    }
+    return 0;
+}
